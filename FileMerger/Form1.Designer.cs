2c86c52308d56d2d72a1c2af00010ad90f741396
Vlado <vlado@consultcommerce.com>
2017-04-11 13:02:49 +0300

    1st commit
    1st commit

---
+++ FileMerger/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace FileMerger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbSource = new System.Windows.Forms.TextBox();
+            this.tbDestination = new System.Windows.Forms.TextBox();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source Directory:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Destination Directory:";
+            // 
+            // tbSource
+            // 
+            this.tbSource.Location = new System.Drawing.Point(138, 89);
+            this.tbSource.Name = "tbSource";
+            this.tbSource.Size = new System.Drawing.Size(385, 20);
+            this.tbSource.TabIndex = 2;
+            // 
+            // tbDestination
+            // 
+            this.tbDestination.Location = new System.Drawing.Point(138, 139);
+            this.tbDestination.Name = "tbDestination";
+            this.tbDestination.Size = new System.Drawing.Size(385, 20);
+            this.tbDestination.TabIndex = 3;
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Location = new System.Drawing.Point(16, 297);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(105, 23);
+            this.btnMerge.TabIndex = 4;
+            this.btnMerge.Text = "Merge!";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 13);
+            this.label3.MaximumSize = new System.Drawing.Size(500, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(488, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Use Merge! to merge all files in a directory to a new directory! Files are copied" +
+    " and the original files are preserved. Use only ONCE per directory!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 332);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.tbDestination);
+            this.Controls.Add(this.tbSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "File Merger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbSource;
+        private System.Windows.Forms.TextBox tbDestination;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FileMerger/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FileMerger
+{
+    public partial class Form1 : Form
+    {
+
+        public static List<string> AllFilesList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            int index = 1;
+            DirSearch(tbSource.Text);
+
+            string sourcePath = tbSource.Text;
+            string targetPath = tbDestination.Text;
+
+            if (Directory.Exists(sourcePath))
+            {
+                foreach (string str in Directory.GetFiles(sourcePath))
+                {
+                    AllFilesList.Add(str);
+                }
+
+                foreach (string s in AllFilesList)
+                {
+                                      
+                    string fileName = Path.GetFileName(s);
+                    string destFile = Path.Combine(targetPath, fileName);
+                   // File.Copy(s, destFile, false);
+                    try
+                    {
+                        File.Copy(s, destFile, false);
+                    }
+                    catch (Exception)
+                    {
+                        try
+                        {
+                            List<string> temp = fileName.Split('.').ToList();
+                            File.Copy(s, Path.Combine(targetPath, temp[0] + "(" + index + ")." + temp[1]), false);
+                        }
+                        catch (Exception)
+                        {
+                            index++;
+                            List<string> temp = fileName.Split('.').ToList();
+                            File.Copy(s, Path.Combine(targetPath, temp[0] + "(" + index + ")." + temp[1]), false);
+                        }
+                      
+                    }
+                   
+                }
+                MessageBox.Show("the merging is complete", "Protoss Archon",
+                   MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Source path does not exist!", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        public static void DirSearch(string sDir)
+        {
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    foreach (string f in Directory.GetFiles(d))
+                    {
+                        AllFilesList.Add(f);
+                    }
+                    DirSearch(d);
+                }
+            }
+            catch (Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+    }
+}
